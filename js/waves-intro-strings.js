ae7f7f75f37968323874ab7b23e1f60d0a3b148e
samreid <reids@colorado.edu>
2020-02-27 12:03:45 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/waves-intro-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import wavesIntro from './wavesIntro.js';
+
+const wavesIntroStrings = getStringModule( 'WAVES_INTRO' );
+
+wavesIntro.register( 'wavesIntroStrings', wavesIntroStrings );
+
+export default wavesIntroStrings;
