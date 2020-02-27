d52daf3b52ab6dfdd26faadebbddf10ab803155f
samreid <reids@colorado.edu>
2020-02-27 11:46:36 -0700

    ES6 module migration, see https://github.com/phetsims/chipper/issues/875

--- js/waves-intro-main.js
+++ js/waves-intro-main.js
@@ -5,81 +5,76 @@
  *
  * @author Sam Reid (PhET Interactive Simulations)
  */
-define( require => {
-  'use strict';
 
-  // modules
-  const AlignGroup = require( 'SCENERY/nodes/AlignGroup' );
-  const Image = require( 'SCENERY/nodes/Image' );
-  const MediumScreen = require( 'WAVE_INTERFERENCE/common/MediumScreen' );
-  const platform = require( 'PHET_CORE/platform' );
-  const Sim = require( 'JOIST/Sim' );
-  const SimLauncher = require( 'JOIST/SimLauncher' );
-  const soundManager = require( 'TAMBO/soundManager' );
+import Sim from '../../joist/js/Sim.js';
+import SimLauncher from '../../joist/js/SimLauncher.js';
+import platform from '../../phet-core/js/platform.js';
+import AlignGroup from '../../scenery/js/nodes/AlignGroup.js';
+import Image from '../../scenery/js/nodes/Image.js';
+import soundManager from '../../tambo/js/soundManager.js';
+import MediumScreen from '../../wave-interference/js/common/MediumScreen.js';
+import lightScreenIcon from '../images/light_screen_icon_png.js';
+import soundScreenIcon from '../images/sound_screen_icon_png.js';
+import waterScreenIcon from '../images/water_screen_icon_png.js';
+import wavesIntroStrings from './waves-intro-strings.js';
 
-  // strings
-  const lightString = require( 'string!WAVES_INTRO/light' );
-  const soundString = require( 'string!WAVES_INTRO/sound' );
-  const waterString = require( 'string!WAVES_INTRO/water' );
-  const wavesIntroTitleString = require( 'string!WAVES_INTRO/waves-intro.title' );
+const lightString = wavesIntroStrings.light;
+const soundString = wavesIntroStrings.sound;
+const waterString = wavesIntroStrings.water;
+const wavesIntroTitleString = wavesIntroStrings[ 'waves-intro' ].title;
 
-  // images
-  const lightScreenIcon = require( 'image!WAVES_INTRO/light_screen_icon.png' );
-  const soundScreenIcon = require( 'image!WAVES_INTRO/sound_screen_icon.png' );
-  const waterScreenIcon = require( 'image!WAVES_INTRO/water_screen_icon.png' );
 
-  const simOptions = {
-    credits: {
-      leadDesign: 'Amy Rouinfar, Noah Podolefsky',
-      softwareDevelopment: 'Sam Reid',
-      team: 'Wendy Adams, Diana L\u00f3pez Tavares, Ariel Paul, Kathy Perkins, Katie Woessner',
-      qualityAssurance: 'Steele Dalton, Megan Lai, Liam Mulhall, Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz',
-      graphicArts: 'Cheryl McCutchan',
-      thanks: 'This material is based upon work supported by the National Science Foundation under STROBE Grant No. DMR ' +
-              '1548924. Any opinions, findings, and conclusions or recommendations expressed in this material are those ' +
-              'of the author(s) and do not necessarily reflect the views of the National Science Foundation.'
-    },
-    webgl: platform.mobileSafari
-  };
+const simOptions = {
+  credits: {
+    leadDesign: 'Amy Rouinfar, Noah Podolefsky',
+    softwareDevelopment: 'Sam Reid',
+    team: 'Wendy Adams, Diana L\u00f3pez Tavares, Ariel Paul, Kathy Perkins, Katie Woessner',
+    qualityAssurance: 'Steele Dalton, Megan Lai, Liam Mulhall, Laura Rea, Jacob Romero, Katie Woessner, Kelly Wurtz',
+    graphicArts: 'Cheryl McCutchan',
+    thanks: 'This material is based upon work supported by the National Science Foundation under STROBE Grant No. DMR ' +
+            '1548924. Any opinions, findings, and conclusions or recommendations expressed in this material are those ' +
+            'of the author(s) and do not necessarily reflect the views of the National Science Foundation.'
+  },
+  webgl: platform.mobileSafari
+};
 
-  // launch the sim - beware that scenery Image nodes created outside of SimLauncher.launch() will have zero bounds
-  // until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
-  SimLauncher.launch( () => {
+// launch the sim - beware that scenery Image nodes created outside of SimLauncher.launch() will have zero bounds
+// until the images are fully loaded, see https://github.com/phetsims/coulombs-law/issues/70
+SimLauncher.launch( () => {
 
-    // Panels on the right side of the lattice (in the first three screens) have matching widths, within each screen and
-    // across screens.
-    const alignGroup = new AlignGroup( {
+  // Panels on the right side of the lattice (in the first three screens) have matching widths, within each screen and
+  // across screens.
+  const alignGroup = new AlignGroup( {
 
-      // Elements should have the same widths but not constrained to have the same heights
-      matchVertical: false
-    } );
-
-    const screens = [
-      new MediumScreen( alignGroup, {
-        scenes: [ 'waterScene' ],
-        name: waterString,
-        homeScreenIcon: new Image( waterScreenIcon ),
-        supportsSound: true
-      } ),
-      new MediumScreen( alignGroup, {
-        scenes: [ 'soundScene' ],
-        name: soundString,
-        homeScreenIcon: new Image( soundScreenIcon ),
-        showPlaySoundButton: true,
-        supportsSound: true
-      } ),
-      new MediumScreen( alignGroup, {
-        scenes: [ 'lightScene' ],
-        name: lightString,
-        homeScreenIcon: new Image( lightScreenIcon ),
-        supportsSound: true
-      } )
-    ];
-
-    const sim = new Sim( wavesIntroTitleString, screens, simOptions );
-    sim.start();
-
-    // Must be done after sound manager initialized
-    soundManager.setOutputLevelForCategory( 'user-interface', 0.15 );
+    // Elements should have the same widths but not constrained to have the same heights
+    matchVertical: false
   } );
+
+  const screens = [
+    new MediumScreen( alignGroup, {
+      scenes: [ 'waterScene' ],
+      name: waterString,
+      homeScreenIcon: new Image( waterScreenIcon ),
+      supportsSound: true
+    } ),
+    new MediumScreen( alignGroup, {
+      scenes: [ 'soundScene' ],
+      name: soundString,
+      homeScreenIcon: new Image( soundScreenIcon ),
+      showPlaySoundButton: true,
+      supportsSound: true
+    } ),
+    new MediumScreen( alignGroup, {
+      scenes: [ 'lightScene' ],
+      name: lightString,
+      homeScreenIcon: new Image( lightScreenIcon ),
+      supportsSound: true
+    } )
+  ];
+
+  const sim = new Sim( wavesIntroTitleString, screens, simOptions );
+  sim.start();
+
+  // Must be done after sound manager initialized
+  soundManager.setOutputLevelForCategory( 'user-interface', 0.15 );
 } );
