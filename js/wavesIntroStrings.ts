419c5bd0c5686431e57f027d523b6b3170e63c95
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/wavesIntroStrings.ts
+++ js/wavesIntroStrings.ts
@@ -5,15 +5,20 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import wavesIntro from './wavesIntro.js';
 
 type StringsType = {
   'waves-intro': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'water': string;
+  'waterProperty': TReadOnlyProperty<string>;
   'sound': string;
+  'soundProperty': TReadOnlyProperty<string>;
   'light': string;
+  'lightProperty': TReadOnlyProperty<string>;
 };
 
 const wavesIntroStrings = getStringModule( 'WAVES_INTRO' ) as StringsType;
